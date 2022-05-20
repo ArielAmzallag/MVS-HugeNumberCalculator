b072a64f5548f028b203da8489853e4b809b82c5
ArielAmzallag <ariel.amzallag@efrei.net>
2022-05-20 12:45:00 +0200

    Preparation Main

--- FizzBuzz.c
+++ FizzBuzz.c
@@ -4,22 +4,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "HugeUnsignedInt.h"
+#include "ShowHugeNumber.h"
 
 #define FIZZ "Fizz"
 #define BUZZ "Buzz"
 
 int main (void) {
-    for (int i = 1; i <= 100; i++) {
-        if (i % 15 == 0) {
-            printf ("%s%s\n", FIZZ, BUZZ);
-        } else if (i % 3 == 0) {
-            printf ("%s\n", FIZZ);
-        } else if (i % 5 == 0) {
-            printf ("%s\n", BUZZ);
-        } else {
-            printf ("%d\n", i);
-        }
-    }
-
-    return EXIT_SUCCESS;
+    HugeUnsignedInt* hugeNumber = createHugeUnsignedIntFromString ("123456789012345678901234567890");
+    printHugeUnsignedInt (hugeNumber);
+    hugeNumber = createHugeUnsignedIntFromString ("0000000123456789012345678901234567890");
+    printHugeUnsignedInt (hugeNumber);
 }
